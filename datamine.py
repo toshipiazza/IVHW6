c6e354e5f906e51d5c799c6a041c3978e0e51c6a
Toshi Piazza <toshi.piazza@gmail.com>
2016-03-08 22:42:10 -0500

    Adds python template

--- datamine.py
+++ datamine.py
@@ -4,3 +4,13 @@
 # we use the csv format defined by
 #   http://bl.ocks.org/WillTurman/4631136
 # <key>,<value>,<date>
+
+def datamineRepo(repo):
+    pass
+
+if __name__=='__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--repo', help='url of repo to datamine')
+    args = parser.parse_args()
+    datamineRepo(args.repo)
