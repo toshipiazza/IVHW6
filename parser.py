48abf118ab51ab14e3205c2e637f740b26e8a7b1
Jake Martin <jmart.alb@gmail.com>
2016-03-09 02:31:30 -0500

    compiled normalized data. Do we want to filter it so that files without many changes aren't included?

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ import os
 import sys
 import string
 import csv
+import math
 
 def num(s):
     try:
@@ -9,8 +10,12 @@ def num(s):
     except ValueError:
         return -1
 
-rails_max = 45904
-django_max = 10346
+rails_max = 45904.0
+django_max = 10346.0
+current_month = 3
+current_year = 16
+current_day = "10"
+
 if __name__ == '__main__':
     #creates file named temp.dot
     #argv[0] is the file
@@ -19,29 +24,40 @@ if __name__ == '__main__':
     if (len(sys.argv) == 2):
         commitsfile = sys.argv[1]
         #open commits to read
-        with open(commitsfile) as f:
-            #for each month aka a line in commits file
-            for month in f:
-                file = ""
-                date = ""
-                files = month.split("|")
-                for s in range(0,len(files)):
-                    if (s == 0):
-                        temp = files[s].split(" month ago ")
-                        date = temp[0]
-                        if (len(temp) > 1):
-                            file = temp[1]
-                    else:
-                        temp = files[s].split(" ")
-                        if (temp[1] != "Bin"):
-                            number = num(temp[1])
-                            if (number > number_max):
-                                number_max = number
-                            if (number == -1):
-                                #something went wrong with the parser
-                                print("debug stuff: ")
-                                print(temp)
-                        #put in csv here
-                        filename = temp[3]
-                        continue
+        with open('djangodata.csv', 'wb') as csvfile:
+            spamwriter = csv.writer(csvfile)
+            with open(commitsfile) as f:
+                #for each month aka a line in commits file
+                for month in f:
+                    file = ""
+                    date = ""
+                    files = month.split("|")
+                    for s in range(0,len(files)):
+                        if (s == 0):
+                            temp = files[s].split(" month ago ")
+                            if (len(temp) > 1):
+                                file = temp[1]
+                                months_ago = int(temp[0])
+                                new_month = current_month - months_ago + 1
+                                if (new_month <= 0):
+                                    year = int(current_year - math.floor((new_month*-1) / 12) - 1)
+                                    month = 12 - ((new_month*-1) % 12)
+                                    date = str(month) + "/" + current_day + "/" + str(year)
+                                else:
+                                    date = str(new_month) + "/" + current_day + "/" + str(current_year)
+                        else:
+                            temp = files[s].split(" ")
+                            if (temp[1] != "Bin"):
+                                number = num(temp[1])
+                                if (number > number_max):
+                                    number_max = number
+                                if (number == -1):
+                                    #something went wrong with the parser
+                                    print("debug stuff: ")
+                                    print(temp)
+                            filename = temp[3]
+                            formatted_number = str("{0:.3f}".format(number/django_max))
+                            #put in csv here
+                            spamwriter.writerow([filename,formatted_number,date])
+                            continue
         print("max number is " + str(number_max))
