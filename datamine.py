8aa01af7de124c41466d3a9e75311f6ade164292
Toshi Piazza <toshi.piazza@gmail.com>
2016-03-08 22:36:08 -0500

    Adds preliminary files for csv output

---
+++ datamine.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python2
+
+# prints out a csv representing what files changed and when
+# we use the csv format defined by
+#   http://bl.ocks.org/WillTurman/4631136
+# <key>,<value>,<date>
