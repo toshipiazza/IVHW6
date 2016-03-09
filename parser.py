890350fc34534c42991677e064f162bca9e50492
Jake Martin <jmart.alb@gmail.com>
2016-03-09 17:02:15 -0500

    Changed to file extension. Fixed some bugs with parsing. (non line based files, files with 0 lines changed, etc)

--- parser.py
+++ parser.py
@@ -10,8 +10,8 @@ def num(s):
     except ValueError:
         return -1
 
-rails_max = 45904.0
-django_max = 10346.0
+rails_max = 405662.0
+django_max = 520017.0
 current_month = 3
 current_year = 16
 current_day = "10"
@@ -24,19 +24,21 @@ if __name__ == '__main__':
     if (len(sys.argv) == 2):
         commitsfile = sys.argv[1]
         #open commits to read
-        with open('djangodata.csv', 'wb') as csvfile:
+        with open('djangodata.csv', 'w') as csvfile:
             spamwriter = csv.writer(csvfile)
             with open(commitsfile) as f:
                 #for each month aka a line in commits file
                 for month in f:
                     file = ""
                     date = ""
+                    filename = ""
+                    extensions = {}
                     files = month.split("|")
                     for s in range(0,len(files)):
                         if (s == 0):
                             temp = files[s].split(" month ago ")
                             if (len(temp) > 1):
-                                file = temp[1]
+                                filename = temp[1]
                                 months_ago = int(temp[0])
                                 new_month = current_month - months_ago + 1
                                 if (new_month <= 0):
@@ -49,15 +51,38 @@ if __name__ == '__main__':
                             temp = files[s].split(" ")
                             if (temp[1] != "Bin"):
                                 number = num(temp[1])
-                                if (number > number_max):
-                                    number_max = number
                                 if (number == -1):
                                     #something went wrong with the parser
                                     print("debug stuff: ")
                                     print(temp)
-                            filename = temp[3]
-                            formatted_number = str("{0:.3f}".format(number/django_max))
-                            #put in csv here
-                            spamwriter.writerow([filename,formatted_number,date])
+                                if (filename == ""):
+                                    filename = files[s-1].split(" ")[2]
+                                filename, ext = os.path.splitext(filename)
+                                if (ext == ""):
+                                    if (filename != ""):
+                                        ext = os.path.basename(os.path.normpath(filename))
+                                if (extensions.has_key(ext)):
+                                    number = extensions[ext] + number
+                                    extensions[ext] = number
+                                    if (number > number_max):
+                                        number_max = number
+                                else:
+                                    extensions.setdefault(ext, [])
+                                    extensions[ext] = (number)
+                                if (number == 0):
+                                    filename = temp[2]
+
+                                else:
+                                    filename = temp[3]
+
+                            else:
+                                #dont record anything
+                                filename = temp[6]
+                                continue
                             continue
+
+                    for key in extensions:
+                        #put in csv here
+                        formatted_number = str("{0:.3f}".format(extensions[key]/django_max))
+                        spamwriter.writerow([key,formatted_number,date])
         print("max number is " + str(number_max))
